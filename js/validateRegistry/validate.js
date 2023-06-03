208827b6c56997997a374930e4d411d6a0558118
alexus21 <US21003@ues.edu.sv>
2023-06-03 12:40:18 -0600

    Agregando validaciones a correo; pendiente de resolver 'endsWithCom' (bug) y 'emailHasChars' (no soluciona el guion medio)

--- js/validateRegistry/validate.js
+++ js/validateRegistry/validate.js
@@ -11,6 +11,7 @@ const validateRegistry = (e) =>{
 
     //Call function to validate username:
     checkIfUsernameIsCorrect(username);
+    checkIfEmailIsCorrect(email);
 
 };
 
@@ -63,6 +64,73 @@ const checkIfUsernameIsCorrect = (username) => {
     invalidUsername.classList.add("invalid-feedback");
 };
 
+const checkIfEmailIsCorrect = (email) => {
+    const invalidEmail = document.querySelector("[data-invalid-email]");
+
+    // Si no se ha proveído nombre de usuario alguno:
+    if(!email){
+        invalidEmail.innerHTML = "El correo es obligatorio";
+        invalidEmail.classList.remove("invalid-feedback");
+        registryButton.disabled = true;
+        return;
+    }
+
+    if(emailHasNumbersAtBeginning(email)){
+        invalidEmail.innerHTML = "El correo no debe tener números al inicio";
+        invalidEmail.classList.remove("invalid-feedback");
+        registryButton.disabled = true;
+        return;
+    }
+
+    if(emailHasUnderscoreBeginning(email)){
+        invalidEmail.innerHTML = "El correo no debe tener guiones bajos al inicio";
+        invalidEmail.classList.remove("invalid-feedback");
+        registryButton.disabled = true;
+        return;
+    }
+
+    if(emailHasChars(email)){
+        invalidEmail.innerHTML = "El correo no debe contener caracteres especiales";
+        invalidEmail.classList.remove("invalid-feedback");
+        registryButton.disabled = true;
+        return;
+    }
+
+    if(emailEndsWithCom(email)){
+        invalidEmail.innerHTML = "El correo debe terminar en '.com'";
+        invalidEmail.classList.remove("invalid-feedback");
+        registryButton.disabled = true;
+        return;
+    }
+
+    if(emailHasUppercaseLetters(email)){
+        invalidEmail.innerHTML = "El correo no debe contener mayúsculas";
+        invalidEmail.classList.remove("invalid-feedback");
+        registryButton.disabled = true;
+        return;
+    }
+
+    if(emailHasSpaces(email)){
+        invalidEmail.innerHTML = "El correo no debe contener espacios";
+        invalidEmail.classList.remove("invalid-feedback");
+        registryButton.disabled = true;
+        return;
+    }
+
+    if(emailHasNoAtSymbol(email)){
+        invalidEmail.innerHTML = "El correo no contiene ninguna '@'";
+        invalidEmail.classList.remove("invalid-feedback");
+        registryButton.disabled = true;
+        return;
+    }
+
+    // Resetear en caso de no haber errores:
+    registryButton.disabled = false;
+    invalidEmail.innerHTML = "";
+    invalidEmail.classList.add("invalid-feedback");
+};
+
+//--------------------------- Validar nombres de usuario -------------------------------------
 // Comprobar que los números del nombre de usuario estén al final del nombre:
 const usernameHasNumbersAtBeginning = (username) => {
     const regex = /^[0-9]/;
@@ -81,10 +149,56 @@ const doesUsernameHasSpaces = (username) => {
 
 // Verificar que no contenga caracteres especiales:
 const usernameHasCharacters = (username) => {
-    const availableChars = "~`!@#$%^&*()_+={[}]|\\:;\"'<,>.?-";
+    const availableChars = "-~`!@#$%^&*()_+={[}]|\\:;\"'<,>.?-";
     const regex = new RegExp(`[${availableChars}]`);
 
     return regex.test(username);
+};
+
+// ------------------------------------ Validar correo ------------------------------------------
+
+//Verificar si tiene números al inicio:
+const emailHasNumbersAtBeginning = (email) => {
+    const regex = /^[0-9]/;
+    return regex.test(email);
+};
+
+//Verificar si tiene números al inicio:
+const emailHasUnderscoreBeginning = (email) => {
+    const regex = /^_/;
+    return regex.test(email);
+};
+
+//Verificar si posee caracteres especiales
+const emailHasChars = (email) => {
+    const availableChars = "~`!#$%^&*()+={[}]|\\:;\"'<,>?-";
+    const regex = new RegExp(`[${availableChars}]`);
+
+    return regex.test(email);
+};
+
+//Verificar si termina en ".com"
+const emailEndsWithCom = (email) => {
+    const regex = /\*gmail.com$/i;
+    return regex.test(email);
+};
+
+// Verificar si el correo contiene mayúsculas
+const emailHasUppercaseLetters = (email) => {
+    const regex = /[A-Z]/;
+    return regex.test(email);
+};
+
+// Verificar si el correo contiene espacios
+const emailHasSpaces = (email) => {
+    const regex = /\s/;
+    return regex.test(email);
+};
+
+// Verificar si contiene la arroba "@"
+const emailHasNoAtSymbol = (email) => {
+    const regex = /@/;
+    return !regex.test(email);
 };
 
 export default validateRegistry;
