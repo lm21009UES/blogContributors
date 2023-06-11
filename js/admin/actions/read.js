a7f2dfa25ccbfc7f6711c20c182b5b1265a99d0b
alexus21 <US21003@ues.edu.sv>
2023-06-10 20:38:19 -0600

    Agregados botones a cada fila de la tabla

--- js/admin/actions/read.js
+++ js/admin/actions/read.js
@@ -14,6 +14,10 @@ const tableRow = document.createElement("tr");
 // Encabezados
 const thUsername = document.createElement("th");
 const thEmail = document.createElement("th");
+const thUserRol = document.createElement("th");
+const thEditButton = document.createElement("th");
+const thMuteButton = document.createElement("th");
+const thDeleteButton = document.createElement("th");
 
 // Asignar texto a los encabezados
 thUsername.textContent = "Nombre de usuario";
@@ -22,10 +26,13 @@ thEmail.textContent = "Correo";
 // Agregar los encabezados a la fila
 tableRow.appendChild(thUsername);
 tableRow.appendChild(thEmail);
+tableRow.appendChild(thUserRol);
+tableRow.appendChild(thEditButton);
+tableRow.appendChild(thMuteButton);
+tableRow.appendChild(thDeleteButton);
 
 // Agregar la fila a la tabla
 myTable.appendChild(tableRow);
-
 
 /**
  * La función readData lee los datos almacenados en localStorage y los muestra en una tabla.
@@ -36,19 +43,43 @@ export const readData = () => {
     if (dataList && dataList.length > 0) {
         for (let i = 0; i < dataList.length; i++) {
 
+            // Agregar los botones interactivos de actualizar, borrar y silenciar al usuario.
+            const updateButton = document.createElement("button");
+            updateButton.classList.add("btn", "btn-info");
+            updateButton.textContent = "Editar";
+
+            const muteButton = document.createElement("button");
+            muteButton.classList.add("btn", "btn-warning");
+            muteButton.textContent = "Silenciar";
+
+            const deleteButton = document.createElement("button");
+            deleteButton.classList.add("btn", "btn-danger");
+            deleteButton.textContent = "Eliminar";
+
             // Crear una nueva fila para cada elemento de datos
             const tableRow = document.createElement("tr");
 
+            // Crear celdas
             const tdUsername = document.createElement("td");
             const tdEmail = document.createElement("td");
+            const tdPromoteUser = document.createElement("td");
+            const tdEditInfo = document.createElement("td");
+            const tdMuteUser = document.createElement("td");
+            const tdDeleteUser = document.createElement("td");
 
             // Asignar el contenido de los datos a las celdas correspondientes
             tdUsername.textContent = dataList[i].username;
             tdEmail.textContent = dataList[i].email;
+            tdEditInfo.appendChild(updateButton);
+            tdMuteUser.appendChild(muteButton);
+            tdDeleteUser.appendChild(deleteButton);
 
             // Agregar las celdas a la fila
             tableRow.appendChild(tdUsername);
             tableRow.appendChild(tdEmail);
+            tableRow.appendChild(tdEditInfo);
+            tableRow.appendChild(tdMuteUser);
+            tableRow.appendChild(tdDeleteUser);
 
             // Agregar la fila a la tabla
             myTable.appendChild(tableRow);
