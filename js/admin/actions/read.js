a6ff6003c6754ef3bb8bb15febda52980ba69cad
alexus21 <US21003@ues.edu.sv>
2023-06-11 00:22:54 -0600

    fqwefwer

--- js/admin/actions/read.js
+++ js/admin/actions/read.js
@@ -12,24 +12,20 @@ myTable.classList.add("table", "table-bordered");
 const tableRow = document.createElement("tr");
 
 // Encabezados
+const thCheckBox = document.createElement("th");
 const thUsername = document.createElement("th");
 const thEmail = document.createElement("th");
 const thUserRol = document.createElement("th");
-const thEditButton = document.createElement("th");
-const thMuteButton = document.createElement("th");
-const thDeleteButton = document.createElement("th");
 
 // Asignar texto a los encabezados
 thUsername.textContent = "Nombre de usuario";
 thEmail.textContent = "Correo";
 
 // Agregar los encabezados a la fila
+tableRow.appendChild(thCheckBox);
 tableRow.appendChild(thUsername);
 tableRow.appendChild(thEmail);
 tableRow.appendChild(thUserRol);
-tableRow.appendChild(thEditButton);
-tableRow.appendChild(thMuteButton);
-tableRow.appendChild(thDeleteButton);
 
 // Agregar la fila a la tabla
 myTable.appendChild(tableRow);
@@ -43,43 +39,24 @@ export const readData = () => {
     if (dataList && dataList.length > 0) {
         for (let i = 0; i < dataList.length; i++) {
 
-            // Agregar los botones interactivos de actualizar, borrar y silenciar al usuario.
-            const updateButton = document.createElement("button");
-            updateButton.classList.add("btn", "btn-info");
-            updateButton.textContent = "Editar";
-
-            const muteButton = document.createElement("button");
-            muteButton.classList.add("btn", "btn-warning");
-            muteButton.textContent = "Silenciar";
-
-            const deleteButton = document.createElement("button");
-            deleteButton.classList.add("btn", "btn-danger");
-            deleteButton.textContent = "Eliminar";
-
             // Crear una nueva fila para cada elemento de datos
             const tableRow = document.createElement("tr");
 
             // Crear celdas
+            const tdCheckBox = document.createElement("td");
             const tdUsername = document.createElement("td");
             const tdEmail = document.createElement("td");
             const tdPromoteUser = document.createElement("td");
-            const tdEditInfo = document.createElement("td");
-            const tdMuteUser = document.createElement("td");
-            const tdDeleteUser = document.createElement("td");
 
             // Asignar el contenido de los datos a las celdas correspondientes
+            tdCheckBox.appendChild(createCheckBox());
             tdUsername.textContent = dataList[i].username;
             tdEmail.textContent = dataList[i].email;
-            tdEditInfo.appendChild(updateButton);
-            tdMuteUser.appendChild(muteButton);
-            tdDeleteUser.appendChild(deleteButton);
 
             // Agregar las celdas a la fila
+            tableRow.appendChild(tdCheckBox);
             tableRow.appendChild(tdUsername);
             tableRow.appendChild(tdEmail);
-            tableRow.appendChild(tdEditInfo);
-            tableRow.appendChild(tdMuteUser);
-            tableRow.appendChild(tdDeleteUser);
 
             // Agregar la fila a la tabla
             myTable.appendChild(tableRow);
@@ -89,3 +66,12 @@ export const readData = () => {
     // Agregar la tabla al contenedor especificado en el HTML
     tableContainer.appendChild(myTable);
 };
+
+const createCheckBox = () => {
+    // crear checkbox con label
+    const myCheckBox = document.createElement("input");
+    myCheckBox.type = "checkbox";
+    myCheckBox.classList.add("form-check-input");
+    return myCheckBox;
+};
+
--- js/admin/event.js
+++ js/admin/event.js
@@ -1,7 +1,17 @@
-import { readData } from "./actions/read.js";
+import {readData} from "./actions/read.js";
+import createNewUser from "./actions/create.js";
+
+const btnAddUser = document.querySelector("[add-user-button]");
+const btnEditUser = document.querySelector("[edit-info-button]");
+const btnMuteUser = document.querySelector("[mute-user-button]");
+const btnDeleteUser = document.querySelector("[delete-user-button]");
 
 document.addEventListener("DOMContentLoaded", function() {
     readData();
+    btnEditUser.disabled = true;
+    btnMuteUser.disabled = true;
+    btnDeleteUser.disabled = true;
 });
 
-
+// btnAdd.addEventListener('click', createNewUser);
+btnAddUser.addEventListener("click", createNewUser);
