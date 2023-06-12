2155c1d2cc631423065dff31d7c46a7112239304
alexus21 <US21003@ues.edu.sv>
2023-06-11 21:52:04 -0600

    Agregado evento de eliminar elementos de localStorage

--- js/admin/actions/delete.js
+++ js/admin/actions/delete.js
@@ -1,9 +1,16 @@
-const checkboxes = document.querySelectorAll("input[type='checkbox']");
+let myCheckedIndexes = [];
 
-export const enableButtons = () => {
-    checkboxes.forEach(function (checkbox) {
-        if (checkbox.checked) {
-            alert("Checked");
+export const getItems = (checkedIndexes) => {
+    myCheckedIndexes = checkedIndexes;
+};
+
+export const deleteItems = (checkedIndexes) => {
+    const row = document.querySelector("table");
+
+    for (let i = 0; i < checkedIndexes.length; i++) {
+        if (row) {
+            row.remove();
         }
-    });
+    }
+    return checkedIndexes = [];
 };
--- js/admin/event.js
+++ js/admin/event.js
@@ -1,5 +1,6 @@
 // Importar la función readData del archivo read.js en la carpeta actions
 import { readData } from "./actions/read.js";
+// import {deleteItems, getItems} from "./actions/delete.js";
 
 // Obtener los botones de editar usuario, silenciar usuario, eliminar usuario y cancelar del documento HTML
 const btnEditUser = document.querySelector("[edit-info-button]");
@@ -7,13 +8,25 @@ const btnMuteUser = document.querySelector("[mute-user-button]");
 const btnDeleteUser = document.querySelector("[delete-user-button]");
 const btnCancell = document.querySelector("[cancell-button]");
 
+const checkedBoxIndexes = []; // Array para almacenar los índices de los checkboxes seleccionados
+
+let dataLocalStorage = JSON.parse(localStorage.getItem('database')) || [];
+
+const disableButtons = () => {
+    // Deshabilitar los botones de editar usuario, silenciar usuario y eliminar usuario
+    btnEditUser.disabled = btnMuteUser.disabled = btnDeleteUser.disabled = true;
+};
+
+const enableButtons = () => {
+    // Deshabilitar los botones de editar usuario, silenciar usuario y eliminar usuario
+    btnEditUser.disabled = btnMuteUser.disabled = btnDeleteUser.disabled = false;
+};
+
 // Ejecutar el código cuando el contenido del documento se haya cargado
 document.addEventListener("DOMContentLoaded", function() {
     // Llamar a la función readData para leer los datos
     readData();
-
-    // Deshabilitar los botones de editar usuario, silenciar usuario y eliminar usuario
-    btnEditUser.disabled = btnMuteUser.disabled = btnDeleteUser.disabled = true;
+    disableButtons();
 });
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -21,32 +34,48 @@ document.addEventListener("DOMContentLoaded", function() {
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
     // Función para contar la cantidad de checkboxes seleccionados
-    function countCheckedCheckboxes() {
-        let count = 0;
+    const countCheckedCheckboxes = () => {
+        return [...checkboxes].filter(checkbox => checkbox.checked).length;
+    };
 
-        checkboxes.forEach(function(checkbox) {
+    // Función para obtener los índices de los checkboxes seleccionados
+    const checkIndexes = () => {
+        // checkedIndexes.length = 0; // Reiniciar el array de índices seleccionados
+        checkboxes.forEach((checkbox, index) => {
             if (checkbox.checked) {
-                count++;
+                if(!checkedBoxIndexes.includes(index)){
+                    checkedBoxIndexes.push(index);
+                }
             }
         });
+    };
 
-        return count;
-    }
+    // Función para manejar el cambio de estado de los checkboxes
+    const handleCheckboxChange = () => {
+        const checkedCount = countCheckedCheckboxes();
+        checkIndexes();
 
-    // Agregar event listeners a los checkboxes para habilitar o deshabilitar los botones según la cantidad seleccionada
-    checkboxes.forEach(function(checkbox) {
-        checkbox.addEventListener('change', function() {
-            const checkedCount = countCheckedCheckboxes();
+        if (checkedCount === 0) {
+            disableButtons();
+        } else {
+            enableButtons();
 
-            if (checkedCount === 0) {
-                btnEditUser.disabled = true;
-                btnMuteUser.disabled = true;
-                btnDeleteUser.disabled = true;
-            } else {
-                btnEditUser.disabled = btnMuteUser.disabled = btnDeleteUser.disabled = false;
-            }
-        });
+        }
+    };
+
+    // Agregar event listeners a los checkboxes para manejar el cambio de estado
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', handleCheckboxChange);
     });
+});
+
+btnDeleteUser.addEventListener("click", function () {
+    for (let i = 0; i <= checkedBoxIndexes.length -1; i++) {
+        dataLocalStorage.splice(checkedBoxIndexes[i], 1);
+    }
+    localStorage.setItem('database', JSON.stringify(dataLocalStorage));
+    readData();
+    window.location.reload();
 });
 
 // Agregar un event listener al botón de cancelar para recargar la página
