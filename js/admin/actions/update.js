4ad8f14653cf96ad5527340291af0f57f4b08462
alexus21 <US21003@ues.edu.sv>
2023-06-11 22:55:02 -0600

    Refactoring for 'Actualizar'

--- js/admin/actions/update.js
+++ js/admin/actions/update.js
@@ -1,6 +1,38 @@
 const dataLocalStorage = JSON.parse(localStorage.getItem('database')) || [];
 
-export const updateValues = (username, email, password, i) => {
+// Obtener los elementos de entrada de nombre de usuario, correo electrónico, contraseña y contraseña repetida del documento HTML
+const inputUsername = document.querySelector("#editCustomUsername");
+const inputEmail = document.querySelector("#editEmail");
+const inputPassword = document.querySelector("#editPassword");
+const inputRetypedPassword = document.querySelector("#editPasswordAgain");
+
+const confirmButton = document.querySelector("[confirm-button]");
+
+export const updateValues = (checkedBoxIndexes) => {
+    // Recorrer los índices de los checkboxes seleccionados
+    for (let i = 0; i <= checkedBoxIndexes.length - 1; i++) {
+
+        // Obtener el índice del elemento seleccionado
+        const selectedIndex = checkedBoxIndexes[i];
+
+        // Obtener el elemento correspondiente a través del índice
+        const data = dataLocalStorage[selectedIndex];
+
+        if (data) {
+            // Asignar los valores a los input correspondientes
+            inputUsername.value = data.username;
+            inputEmail.value = data.email;
+            inputPassword.value = data.password;
+            inputRetypedPassword.value = data.password;
+
+            confirmButton.addEventListener("click", function (){
+                update(inputUsername.value, inputEmail.value, inputPassword.value, selectedIndex, i);
+            });
+        }
+    }
+};
+
+const update = (username, email, password, i) => {
     dataLocalStorage[i].username = username;
     dataLocalStorage[i].email = email;
     dataLocalStorage[i].password = password;
--- js/admin/event.js
+++ js/admin/event.js
@@ -11,14 +11,6 @@ const btnMuteUser = document.querySelector("[mute-user-button]");
 const btnDeleteUser = document.querySelector("[delete-user-button]");
 const btnCancell = document.querySelector("[cancell-button]");
 
-// Obtener los elementos de entrada de nombre de usuario, correo electrónico, contraseña y contraseña repetida del documento HTML
-const inputUsername = document.querySelector("#editCustomUsername");
-const inputEmail = document.querySelector("#editEmail");
-const inputPassword = document.querySelector("#editPassword");
-const inputRetypedPassword = document.querySelector("#editPasswordAgain");
-
-const confirmButton = document.querySelector("[confirm-button]");
-
 const checkedBoxIndexes = []; // Array para almacenar los índices de los checkboxes seleccionados
 
 // Obtener los datos almacenados en localStorage
@@ -82,27 +74,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 btnEditUser.addEventListener("click", function (){
-    // Recorrer los índices de los checkboxes seleccionados
-    for (let i = 0; i <= checkedBoxIndexes.length - 1; i++) {
-
-        // Obtener el índice del elemento seleccionado
-        const selectedIndex = checkedBoxIndexes[i];
-
-        // Obtener el elemento correspondiente a través del índice
-        const data = dataLocalStorage[selectedIndex];
-
-        if (data) {
-            // Asignar los valores a los input correspondientes
-            inputUsername.value = data.username;
-            inputEmail.value = data.email;
-            inputPassword.value = data.password;
-            inputRetypedPassword.value = data.password;
-
-            confirmButton.addEventListener("click", function (){
-                updateValues(inputUsername.value, inputEmail.value, inputPassword.value, selectedIndex);
-            });
-        }
-    }
+    updateValues(checkedBoxIndexes);
     checkedBoxIndexes.splice(0, checkedBoxIndexes.length);
 });
 
